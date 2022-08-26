e91ba400c836b3477c442e7c2ab4e10f2c30f618
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-26 13:20:46 +0100

    Test invalid form input - Testing invalid email address

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -3,9 +3,23 @@ describe("Newsletter Subscribe Form", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it.only("allows users to subscribe to the email list", () => {
+  it("allows users to subscribe to the email list", () => {
     cy.getByData("email-input").type("emma@aol.com")
     cy.getByData("submit-button").click()
     cy.getByData("success-message").should('exist').contains('emma@aol.com')
+  })
+
+  it("does NOT allow an invalid email address", () => {
+    cy.getByData("email-input").type("emma")
+    cy.getByData("submit-button").click()
+    cy.getByData("success-message").should("not.exist")
+  })
+
+  it.only("does NOT allow already subscribed email addresses", () => {
+    cy.getByData("email-input").type("john@example.com")
+    cy.getByData("submit-button").click()
+    cy.getByData("server-error-message")
+      .should("exist")
+      .contains("already exists. Please use a different email address.")
   })
 })
