2f2a87316a79631043b464480a62cbe6ac64881f
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-26 07:36:53 +0100

    Refractor cypress test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,7 @@
 describe('Home page', () => {
   it('The h1 contains the correct test', () => {
     cy.visit('http://localhost:3000')
-    cy.get('h1').contains('Testing Next.js Applications with Cypress')
+    cy.get("[data-test='hero-heading']")
+      .contains('Testing Next.js Applications with Cypress')
   })
 })
