c8f24533c8c4d0c6992c9e8b9d0c301bfcb94d0d
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-26 13:46:56 +0100

    Organizing and grouping tests with context method

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,19 +3,21 @@ describe('Home page', () => {
     cy.visit("http://localhost:3000")
   })
 
-  it('The h1 contains the correct test', () => {
-    cy.getByData("hero-heading")
-      .contains('Testing Next.js Applications with Cypress')
-  })
-
-  // it.only("the features on the homepage are correct", () => {
-  //   cy.get("dt").eq(0).contains('4 Courses')
-  //   cy.get("dt").eq(1).contains("25+ Lessons")
-  //   cy.get("dt").eq(2).contains("Free and Open Source")
-  // })
-  it("the features on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains('4 Courses')
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
+  context('Hero section', () => {
+    it('The h1 contains the correct test', () => {
+      cy.getByData("hero-heading")
+        .contains('Testing Next.js Applications with Cypress')
+    })
+  
+    // it.only("the features on the homepage are correct", () => {
+    //   cy.get("dt").eq(0).contains('4 Courses')
+    //   cy.get("dt").eq(1).contains("25+ Lessons")
+    //   cy.get("dt").eq(2).contains("Free and Open Source")
+    // })
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains('4 Courses')
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 })
