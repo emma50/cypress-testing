e2e7597179de6bda5192a86c354105b5d4b36550
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-26 16:36:03 +0100

    Test multiple pages with cypress

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -8,16 +8,20 @@ describe('Home page', () => {
       cy.getByData("hero-heading")
         .contains('Testing Next.js Applications with Cypress')
     })
-  
-    // it.only("the features on the homepage are correct", () => {
-    //   cy.get("dt").eq(0).contains('4 Courses')
-    //   cy.get("dt").eq(1).contains("25+ Lessons")
-    //   cy.get("dt").eq(2).contains("Free and Open Source")
-    // })
+
     it("the features on the homepage are correct", () => {
+      // find texts in dt array of elements
       cy.get("dt").eq(0).contains('4 Courses')
       cy.get("dt").eq(1).contains("25+ Lessons")
       cy.get("dt").eq(2).contains("Free and Open Source")
+    })
+  })
+
+  context("Courses section", () => {
+    it.only("Course: Testing Your First Next.js Application", () => {
+      // find elements within [data-test="course-0"] elements
+      cy.getByData("course-0").find('a').eq(3).contains('Get started')
+      cy.getByData("course-0").find('a').eq(3).click()
     })
   })
 })
