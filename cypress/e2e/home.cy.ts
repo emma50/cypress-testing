67992d7e83611627dd2d46ad087957d5b0194bc2
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-08-26 09:29:45 +0100

    Refractor test - Refractoring test with the beforeEach hook

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,12 +1,14 @@
 describe('Home page', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
   it('The h1 contains the correct test', () => {
-    cy.visit('http://localhost:3000')
     cy.get("[data-test='hero-heading']")
       .contains('Testing Next.js Applications with Cypress')
   })
 
   it.only("the features on the homepage are correct", () => {
-    cy.visit("http://localhost:3000")
     cy.get("dt").eq(0).contains('4 Courses')
     cy.get("dt").eq(1).contains("25+ Lessons")
     cy.get("dt").eq(2).contains("Free and Open Source")
